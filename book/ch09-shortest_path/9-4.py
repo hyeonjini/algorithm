de8921137fdef6109e5fedf7c55c67c2c6c1044e
hyeonjin <choihj94@gmail.com>
2021-10-08 23:52:14 +0900

    ch09-shortest_path

--- book/ch09-shortest_path/9-4.py
+++ book/ch09-shortest_path/9-4.py
@@ -1,5 +1,4 @@
 import sys
-import heapq
 
 INF = int(1e9)
 
@@ -7,8 +6,28 @@ input = sys.stdin.readline
 
 n, m = map(int , input().split()) # 회사:n, 경로:m
 
-graph = [[] for _ in range(n)]
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
 
 for _ in range(m):
-    a, b, c = map(int , input().split())
-    graph[a].append((b, c))
+    a, b = map(int , input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+x, k = map(int, input().split())
+
+result = graph[1][k] + graph[k][x]
+
+if result >= INF:
+    print("-1")
+else:
+    print(result)
