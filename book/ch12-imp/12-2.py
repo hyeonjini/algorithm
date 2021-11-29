9ab0bddcf6e8f547d82234cb2960ed5e012ab51c
unknown <choihj94@gmail.com>
2021-11-30 06:20:13 +0900

    solve boj

--- book/ch12-imp/12-2.py
+++ book/ch12-imp/12-2.py
@@ -9,7 +9,7 @@ S = list(input().rstrip())
 
 alphabet = [x for x in S if ord(x) >= 65 and ord(x) <= 90]
 number = [int(x) for x in S if ord(x) >= 48 and ord(x) <= 57]
-# print(alphabet, number)
+
 alphabet.sort()
 
 print(''.join(alphabet) + str(sum(number)))
