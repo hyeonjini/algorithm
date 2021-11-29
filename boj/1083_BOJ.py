8fb61d7fddbe8f5f3b90aeea34d21f6e0810cbf0
unknown <choihj94@gmail.com>
2021-11-30 07:02:18 +0900

    solve boj

--- boj/1083_BOJ.py
+++ boj/1083_BOJ.py
@@ -6,34 +6,32 @@ input = sys.stdin.readline
 
 def solution(n, arr, s):
 
-    sorted_arr = sorted(arr, reverse=True)
-    i = 0
-    start = 0
-    k = 0
-    while s > 0:
-        
-        
-        # print(arr)
-        max_index = arr.index(sorted_arr[i])
-        # print('max:', sorted_arr[i], 'index:', max_index, 's:', s)
-        k = 0
-        for d in range(max_index -1 , -1, -1):
-            if arr[d] > arr[max_index]:
+ 
+    for i in range(n):
+
+        max_value = arr[i]
+        max_index = i
+        for j in range(i + 1, (i + s + 1)):
+            if j >= len(arr):
                 break
-            k += 1
-        # print('k', k)
-        if k <= s and max_index >= 0:
-            j = max_index
-            while j > 0 and (arr[j-1] < arr[j]):
-                arr[j - 1], arr[j] = arr[j], arr[j-1]
-                s -= 1
-                j -= 1
-            
-        i += 1
-    # answer = ''
-    # for a in arr:
-    #     answer += str(a)
-    
+            if max_value < arr[j]:
+                max_value = arr[j]
+                max_index = j
+
+        if max_index == i:
+            continue
+        # swap
+        m = max_index
+        while m > i:
+            arr[m], arr[m-1] = arr[m-1], arr[m]
+            m -= 1
+        s -= (max_index - i) # 이동한 수만큼 s 감소
+
+        # print(arr, s)
+
+        if s == 0:
+            break
+        
     answer = ' '.join(list(map(str, arr)))
     return answer
 
