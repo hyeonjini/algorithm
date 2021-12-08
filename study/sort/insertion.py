f1a34002decd510ea2fb2f5837fc6e1beacc9a82
unknown <choihj94@gmail.com>
2021-12-09 04:31:15 +0900

    study

--- study/sort/insertion.py
+++ study/sort/insertion.py
@@ -6,12 +6,14 @@ def insertion_sort(arr):
         temp = arr[i]
         prev = i - 1
 
-        while prev >= 0 and arr[prev] > temp:
+        while prev >= 0 and arr[prev] > temp: # prev는 n부터 0까지 탐색, temp가 들어갈 위치면 break
             arr[prev + 1] = arr[prev]
             prev -= 1
-        arr[prev + 1] = temp
+
+        arr[prev + 1] = temp # temp가 들어갈 위치에 삽입
 
     return arr
+
 if __name__ == "__main__":
     arr = [9, 4, 3, 5, 6, 21, 12, 2]
     print(insertion_sort(arr))
