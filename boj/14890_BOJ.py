09a00d4149cd964c40a98638ab3bcce51e9f3a23
unknown <choihj94@gmail.com>
2021-12-24 08:28:56 +0900

    solove boj

--- boj/14890_BOJ.py
+++ boj/14890_BOJ.py
@@ -4,10 +4,40 @@
 import sys
 input = sys.stdin.readline
 
-def solution():
-    answer =0
+def solution(n, l, paths):
+    answer = 0
+
+    # 행 path 검사
+    for i in range(n):
+        path = paths[i]
+
+        print(path)
+        
+        if is_path(path):
+            answer += 1
+        
+    # 열 path 검사
+    for r in range(n):
+        row_path = []
+        for j in range(n):
+            row_path.append(paths[j][r])
+
+        print(row_path)
+
+        if is_path(row_path):
+            answer += 1
+        
     return answer
 
-if __name__ == "__main__":
+def is_path(path):
     
-    print(solution())
+    return True
+
+if __name__ == "__main__":
+    n, l = map(int, input().split())
+    paths = []
+
+    for _ in range(n):
+        paths.append(list(map(int , input().split())))
+
+    print(solution(n, l, paths))
