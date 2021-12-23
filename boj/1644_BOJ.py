ab10c04b5051477619a93461222499288dbcf5d1
unknown <choihj94@gmail.com>
2021-12-24 00:43:00 +0900

    solve boj

--- boj/1644_BOJ.py
+++ boj/1644_BOJ.py
@@ -3,11 +3,36 @@
 """
 
 import sys
-input = sys.stdin.input
+input = sys.stdin.readline
 
-def solution():
-    pass
+def solution(n):
+    answer = 0
+
+    # 소수 만들기
+    prime_nums = make_prime_nums(n)
+
+    left = 0
+    start = len(prime_nums) - 1
+
+    while left < start:
+        total = sum(prime_nums)
+        
+        if total == n:
+            continue    
+    return answer
+
+def make_prime_nums(n: int):
+    prime_num = []
+
+    a = [False, False] + [True] * (n - 1)
+    for i in range(2, n+1):
+        if a[i]:
+            prime_num.append(i)
+            for j in range(2*i, n+1, i):
+                a[j] = False
+    return prime_num
 
 if __name__ == "__main__":
+    n = int(input())
 
-    print(solution())
+    print(solution(n))
