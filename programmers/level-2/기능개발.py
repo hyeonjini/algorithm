fd1021a94ab06785d1092a865893f761aea3c507
hyeonjin <choihj94@gmail.com>
2021-12-29 23:36:34 +0900

    solve programmers

---
+++ programmers/level-2/기능개발.py
@@ -0,0 +1,13 @@
+"""
+기능 개발
+"""
+
+def solution(progresses, speeds):
+    answer =[]
+
+    return answer
+
+if __name__ == "__main__":
+    progresses = [93, 30, 55]
+    speeds = [1, 30, 5]
+    print(solution(progresses, speeds))
