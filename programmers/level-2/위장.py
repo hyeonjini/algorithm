554c6a75f1a5ae42079bd6170fad956cc86d6e4b
unknown <choihj94@gmail.com>
2022-01-24 23:27:19 +0900

    solve programmers

--- programmers/level-2/위장.py
+++ programmers/level-2/위장.py
@@ -4,20 +4,38 @@
 from collections import defaultdict
 from itertools import combinations
 def solution(clothes):
-    answer = 0
+    answer = 1
     closet = defaultdict(list)
 
+    # key에 따라 dictionary에 종류 추가
     for value, key in clothes:
         closet[key].append(value)
     
     # print(closet)
 
+    # 의상 집합에서 1가지를 뽑는 경우의수 (부위별 선택)
+    # 각 부위별 조합은 곱하기로 계산
+    # for key in closet.keys():
+    #     # print(closet[key])
+    #     answer *= combination(len(closet[key]) + 1, 1)
+
+    # 모두 안뽑는 경우도 있을 수 있으니(반드시 한가지 부위는 선택) -1
+
+    # 코드 개선 (1가지를 뽑는 경우의 수 nCr은 n!//1!(n-1)! )
+    # 결국 n (종류)
+
     for key in closet.keys():
-        print(closet[key])
-        for i in range(len(closet[key]) + 1):
-            print(list(combinations(closet[key], i)))
-            answer += len(list(combinations(closet[key], i)))
+        answer *= len(closet[key]) + 1
     return answer - 1
+
+def permutation(n):
+    result = 1
+    for i in range(n, 0, -1):
+        result *= i
+    return result
+
+def combination(n, r):
+    return permutation(n) // (permutation(r) * permutation(n-r))
 
 
 if __name__ == "__main__":
